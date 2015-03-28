06fcc5ca6a6ea7af45eebf17826e415572674b9c
9426zl <qtce@163.com>
2015-03-28 12:54:10 +0800

    some addition for pipeline

--- WebRobot/DoubanSpider/MovieSpider/MovieSpider/settings.py
+++ WebRobot/DoubanSpider/MovieSpider/MovieSpider/settings.py
@@ -12,6 +12,7 @@ BOT_NAME = 'MovieSpider'
 
 SPIDER_MODULES = ['MovieSpider.spiders']
 NEWSPIDER_MODULE = 'MovieSpider.spiders'
+ITEM_PIPELINES = ['MovieSpider.pipelines.MoviespiderPipeline']
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'MovieSpider (+http://www.yourdomain.com)'
